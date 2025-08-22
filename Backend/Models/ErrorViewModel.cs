c5b690fcbc29b2a0ac0495f68b5df91fd5cffdda
MonsterLuni <monsterluni@gmail.com>
2025-08-22 20:51:19 +0200

    Fix: Corrected Blueprint that is used for the project

--- Backend/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Document_Editor.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
