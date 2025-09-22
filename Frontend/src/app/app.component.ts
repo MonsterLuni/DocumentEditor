0a5a8976d75b2b3c273e81c4f3025e5cc84c72cb
MonsterLuni <monsterluni@gmail.com>
2025-09-22 19:37:29 +0200

    Feat: Add Environments to safely give secrets to azure

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,10 +2,16 @@ import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TranslateDirective} from '@ngx-translate/core';
 import {HeaderComponent} from '../header/header.component';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, TranslateDirective, HeaderComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  public URL: string;
+  constructor() {
+    this.URL = environment.apiUrl;
+  }
+}
