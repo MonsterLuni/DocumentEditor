79099720287afbecb56728181202e274610caffd
MonsterLuni <monsterluni@gmail.com>
2025-09-23 20:40:29 +0200

    Feat: Add Cors to Frontend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -2,23 +2,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument();
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.AllowAnyOrigin();
+            policy.WithOrigins("https://document-editor.monsterluni.ch");
         });
     options.AddPolicy(name: "Testing",
         policy =>
         {
-            policy.WithOrigins("https://document-editor.monsterluni.ch");
+            policy.AllowAnyOrigin();
         });
 });
 
 var app = builder.Build();
 
+Console.WriteLine($"Current Environment: {app.Environment.EnvironmentName}");
 // Configure the HTTP request pipeline.
 if(app.Environment.IsDevelopment())
 {
@@ -29,6 +32,7 @@ if (app.Environment.IsStaging())
     app.UseCors("Testing");
 }
 
+app.MapControllers();
 app.UseOpenApi();
 app.UseSwaggerUi();
 app.UseHttpsRedirection();
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://backend-documenteditor-fzcjasb6ffbrd6fa.germanywestcentral-01.azurewebsites.net/weatherforecast'
 };
