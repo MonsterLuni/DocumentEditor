db03229a7cd463b6363e536c3b77fb122fe2b240
MonsterLuni <monsterluni@gmail.com>
2025-09-23 19:53:00 +0200

    Feat: Add Cors based on environment in Backend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,26 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddOpenApiDocument();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+        });
+    options.AddPolicy(name: "Testing",
+        policy =>
+        {
+            policy.WithOrigins("https://document-editor.monsterluni.ch");
+        });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if(app.Environment.IsDevelopment())
 {
-
+    app.UseCors();
+}
+if (app.Environment.IsStaging())
+{
+    app.UseCors("Testing");
 }
 
 app.UseOpenApi();
 app.UseSwaggerUi();
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
 app.MapGet("/", context =>
 {
@@ -28,23 +39,4 @@ app.MapGet("/", context =>
     return Task.CompletedTask;
 });
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
